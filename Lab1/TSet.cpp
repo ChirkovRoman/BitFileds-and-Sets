61e124593e8f3776aaef1a6c81a4cc71ef1e42db
ChirkovRoman <romul14603@list.ru>
2018-10-09 11:43:38 +0300

    Несколько опереаторов

--- Lab1/TSet.cpp
+++ Lab1/TSet.cpp
@@ -24,8 +24,60 @@ TSet::TSet(const TSet & s)
 	*this = s;
 }
 
+TSet::TSet(const TBitField & bf)
+{
+	MaxPower = bf.GetBitLen();
+	BitField = bf;
+}
+
+TSet::operator TBitField()
+{
+	return BitField;
+}
+
+int TSet::GetMaxPower(void) const
+{
+	return MaxPower;
+}
+
+void TSet::InsElem(const int n)
+{
+	BitField.SetBit(n);
+}
+
+void TSet::DelElem(const int n)
+{
+	BitField.ClearBit(n);
+}
+
+int TSet::IsMember(const int n) const
+{
+	BitField.GetBit(n);
+}
+
+int TSet::operator==(const TSet & s)
+{
+	if (BitField == s.BitField)
+		return 1;
+	else return 0;
+}
+
 TSet & TSet::operator=(const TSet & s)
 {
 	MaxPower = s.MaxPower;
 	BitField = s.BitField;
+}
+
+TSet TSet::operator+(const int n)
+{
+	TSet tmp = *this;
+	tmp.InsElem(n);
+	return tmp;
+}
+
+TSet TSet::operator-(const int n)
+{
+	TSet tmp = *this;
+	tmp.DelElem(n);
+	return tmp;
 }
