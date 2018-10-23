552eddea9bbdbcecd0e9683930c04e22e273d567
ChirkovRoman <romul14603@list.ru>
2018-10-23 12:02:34 +0300

    Подправил мелочи

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void menu();
 void primeNumbers();
 void operations();
-void oneSet(int n, TSet S);
+void oneSet(int n, TSet &S);
 
 int main()
 {
@@ -62,36 +62,36 @@ void operations()
 {
 	int n;
 	cout << "Ввод множества A" << endl;
-	cout << "Введите наибольший элемент" << endl;
+	cout << "Введите наибольший возможный элемент" << endl;
 	cin >> n;
 	TSet A(n + 1);
 	cin >> A;
 
 	cout << "Введённое множество" << endl;
 	cout << A << endl;
-	cout << "Всего элементов " << A.GetPower() << endl;
+	cout << "Всего элементов " << A.GetPower() << endl << endl;
 
 	cout << "Ввод множества B" << endl;
-	cout << "Введите наибольший элемент" << endl;
+	cout << "Введите наибольший возможный элемент" << endl;
 	cin >> n;
 	TSet B(n + 1);
 	cin >> B;
 
 	cout << "Введённое множество" << endl;
 	cout << B << endl;
-	cout << "Всего элементов " << B.GetPower() << endl;
+	cout << "Всего элементов " << B.GetPower() << endl << endl;
 
-	int input = -1;
+	int input = 0;
 	while (input != -1) {
 		cout << "Выберите дальнейшее действие:" << endl
 			<< "\t1. Операции над A" << endl
 			<< "\t2. Операции над B" << endl
 			<< "\t3. Операции над обоими множествами" << endl
-			<< "\t-1. Завершить работу" << endl;
+			<< "\t-1. Завершить работу c этими множествами" << endl;
 		cin >> input;
 
 		if ((input == 1) || (input == 2)) {
-			cout << "Выберите дальнейшее действие:" << endl
+			cout << endl << "Выберите дальнейшее действие:" << endl
 				<< "\t1. Добавить элемент" << endl
 				<< "\t2. Удалить элемент" << endl
 				<< "\t3. Напечатать дополнение множества" << endl
@@ -105,7 +105,7 @@ void operations()
 		}
 
 		if (input == 3) {
-			cout << "Выберите дальнейшее действие:" << endl
+			cout << endl << "Выберите дальнейшее действие:" << endl
 				<< "\t1. Проверить равенство множеств А и B" << endl
 				<< "\t2. Найти объединение множеств А и B" << endl
 				<< "\t3. Найти пересечение множеств А и B" << endl
@@ -142,23 +142,26 @@ void operations()
 	}
 }
 
-void oneSet(int n, TSet S)
+void oneSet(int n, TSet &S)
 {
 	int k;
 	switch (n) {
 	case 1:
+		cout << "{" << S << "}" << endl;
 		cout << "Введите элемент для добавления в множество" << endl;
 		cin >> k;
 		S = S + k;
-		cout << S << endl;
+		cout << "{" << S << "}" << endl;
 		break;
 	case 2:
+		cout << "{" << S << "}" << endl;
 		cout << "Введите элемент для исключения из множества" << endl;
 		cin >> k;
 		S = S - k;
-		cout << S << endl;
+		cout << "{" << S << "}" << endl;
 		break;
 	case 3:
+		cout << "{" << S << "}" << endl;
 		cout << "Дополнение множества = {" << ~S << "}" << endl;
 		break;
 	case 4:
@@ -168,6 +171,7 @@ void oneSet(int n, TSet S)
 			cout << k << " является элементом множества" << endl;
 		else
 			cout << k << " не является элементом множества" << endl;
+		break;
 	default:
 		cout << "Введено неверное значение" << endl;
 	}
